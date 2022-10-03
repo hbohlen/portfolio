63b0067f47ec48be23e84523114cef71e8ca9553
hbohlen <bohlenhayden@gmail.com>
2022-10-03 00:43:39 -0500

    Updated imports for components after removing typings file

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,5 @@
 import { motion } from 'framer-motion'
 import React from 'react'
-//import { urlFor } from '../sanity';
-//import { Skill } from '../typings';
 
 type Props = {
     directionLeft?: boolean;
