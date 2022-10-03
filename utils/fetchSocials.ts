11a5df171c3ccc7c4e2dc99d02e205db9ec217e4
hbohlen <bohlenhayden@gmail.com>
2022-10-03 18:13:02 -0500

    Added getSocials api call to fetchSocials

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -9,21 +9,12 @@ type Data = {
     socials: Social[]
 }
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-  ) {
-    const socials: Social[] = await sanityClient.fetch(query);
-
-    res.status(200).json({ socials })
-  }
-
 
 export const fetchSocials = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
+    const res = await sanityClient.fetch(query);
 
-    const data = await res.json();
-    const socials: Social[] = data.socials;
+ 
+    const socials: Social[] = res;
 
     console.log("fetching", socials);
 
