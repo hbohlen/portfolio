0370c67c11ff8f17358b3792e1b48cf833ed5c80
hbohlen <bohlenhayden@gmail.com>
2022-10-04 11:24:13 -0500

    Added Tooltip hover for skill components

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import { urlFor } from '../sanity';
 import { Skill } from '../typings';
+import { Tooltip } from '@material-tailwind/react';
 
 type Props = {
   skill: Skill;
@@ -11,18 +12,20 @@ type Props = {
 
 const Skill = ({ skill, directionLeft }: Props) => {
   return (
-    <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{
-          x: directionLeft ? -200 : 200,
-          opacity: 0
-        }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        src={urlFor(skill?.image).url()}
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:32 xl:h-32 filter group-hover:grayscale transition-duration-300 ease-in-out"
-      />
-    </div>
+    <Tooltip content={skill.title}>
+      <div className="group relative flex cursor-pointer">
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          src={urlFor(skill?.image).url()}
+          className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:32 xl:h-32 filter group-hover:grayscale transition-duration-300 ease-in-out"
+        />
+      </div>
+    </Tooltip>
   );
 };
 
