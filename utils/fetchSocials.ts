3a661f83c961d2b355794f9b6fed40e39003dbfa
hbohlen <bohlenhayden@gmail.com>
2022-10-03 19:42:53 -0500

    Adjusted fetchSocials imports

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../sanity";
 
+
 const query = groq`*[_type == "social"]`;
 
 type Data = {
