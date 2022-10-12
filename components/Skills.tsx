6a001f917f7a89a4b093bf97101d71397b99f06a
hbohlen <bohlenhayden@gmail.com>
2022-10-11 21:02:49 -0500

    Added grid-cols-3 for small screens on skills grid

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -12,7 +12,7 @@ const Skills = ({ skills }: Props) => {
     <motion.div className="h-screen w-5/6 flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
 
-      <div className="grid grid-cols-4 gap-5 md:grid-cols-4">
+      <div className="grid grid-cols-4 gap-5 md:grid-cols-3 sm:grid-cols-3">
         {skills?.map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
