2e6f2c2b22da70dd946511b5e0818a9d76f6553c
hbohlen <bohlenhayden@gmail.com>
2022-10-11 20:04:48 -0500

    Fixed mobile skills display issue, set grid columns to 3 on smaller screens instead of 4

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 const Skills = ({ skills }: Props) => {
   return (
-    <motion.div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <motion.div className="h-screen w-5/6 flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
 
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-4 gap-5 md:grid-cols-4">
         {skills?.map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
