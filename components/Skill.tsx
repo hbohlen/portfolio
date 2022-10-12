5d1fa9be82c3da379413bd13b4246cee14b898f4
hbohlen <bohlenhayden@gmail.com>
2022-10-11 21:35:26 -0500

    removed motion divs

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -13,13 +13,7 @@ type Props = {
 const Skill = ({ skill, directionLeft }: Props) => {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{
-          x: directionLeft ? -100 : 100,
-          opacity: 0
-        }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1, x: 0 }}
+      <img
         src={urlFor(skill?.image).url()}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:32 xl:h-32 filter group-hover:grayscale transition-duration-300 ease-in-out"
       />
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,15 +9,15 @@ type Props = {
 
 const Skills = ({ skills }: Props) => {
   return (
-    <motion.div className="h-screen w-5/6 flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <div className="h-screen w-5/6 flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
 
-      <div className="grid grid-cols-4 gap-5 md:grid-cols-3 sm:grid-cols-3">
+      <div className="grid grid-cols-4 gap-5">
         {skills?.map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
       </div>
-    </motion.div>
+    </div>
   );
 };
 
